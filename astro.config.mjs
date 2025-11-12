86a6d916bb642a5a1ed8a24f78363330ef6ba07d
s_lidianzhong <s_lidianzhong@wps.cn>
2025-11-12 16:56:23 +0800

    feat: update style && add github action.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import lightTheme from './src/styles/my-shiki-theme-light.json';
 import darkTheme from './src/styles/my-shiki-theme-dark.json';
 
 export default defineConfig({
+  output: 'static',
   prefetch: true,
   integrations: [react()],
 
